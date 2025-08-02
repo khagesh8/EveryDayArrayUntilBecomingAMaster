2a72f488692243e93c7377d4f47222f13bd2f928
KhageshUmbarkar <khageshumbarkar9@gmail.com>
2025-08-02 15:49:26 +0530

    renamed

--- Day1_FrequencyOfElementsInArray.java
+++ Day1_FrequencyOfElementsInArray.java
@@ -1,5 +1,5 @@
 // Ik the name of the file is not so meaningful, but it's just to keep track on my daily contributions and growth.
-class Day1{
+class Day1_FrequencyOfElementsInArray{
 	public static void main(String[] args) {
 		// Frequncy of elements in an array (no. of times an element is appearing in an array)
 		int [] arr = {1,2,1,1,7};
